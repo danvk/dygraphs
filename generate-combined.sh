43f15770a2542b2b199b936f2e31509f736fe055
Dan Vanderkam <danvdk@gmail.com>
2009-08-19 04:36:16 +0000

    Use a subset of PlotKit_Packed.js -> 30k savings

--- generate-combined.sh
+++ generate-combined.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Generates a single JS file that's easier to include.
 # This packed JS includes a partial copy of MochiKit and PlotKit.
+
+# Generate the packed version of the subset of PlotKit needed by dygraphs.
+# This saves ~30k
+cd plotkit_v091
+./scripts/pack.py Base Layout Canvas > /tmp/plotkit-packed.js
+cd ..
+
 cat \
 mochikit_v14/packed/MochiKit/MochiKit.js \
-plotkit_v091/PlotKit/PlotKit_Packed.js \
+/tmp/plotkit-packed.js \
 dygraph-canvas.js \
 dygraph.js \
 > dygraph-combined.js
