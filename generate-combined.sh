9c97d733b98f043111c92ada7b04f123ebc1693b
Dan Vanderkam <danvdk@gmail.com>
2009-12-01 09:08:53 -0800

    Remove MochiKit dependence and update combined. Down to 45k!!

--- generate-combined.sh
+++ generate-combined.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 # Generates a single JS file that's easier to include.
 
-# Pack up just the bits of MochiKit that we need.
-cd mochikit_v14
-./scripts/pack.py \
-Base \
-> /tmp/mochikit-packed.js
-cd ..
-
 # Pack the dygraphs JS and rgbcolor
 cat \
 dygraph-canvas.js \
@@ -21,7 +14,6 @@ java -jar custom_rhino.jar -c /tmp/dygraph.js \
 # TODO(danvk): ensure the dygraphs copyright, etc. gets into the packed js.
 
 cat \
-/tmp/mochikit-packed.js \
 strftime/strftime-min.js \
 /tmp/dygraph-packed.js \
 > dygraph-combined.js
