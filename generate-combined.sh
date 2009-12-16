f18986e594ec54f8402757b9c087f6a20d611b06
Dan Vanderkam <danvk@google.com>
2009-12-16 07:08:18 -0800

    switch to YUI compressor for making packed JS -- 49k -> 42k

--- generate-combined.sh
+++ generate-combined.sh
@@ -6,14 +6,14 @@ cat \
 dygraph-canvas.js \
 dygraph.js \
 rgbcolor/rgbcolor.js \
+strftime/strftime-min.js \
 > /tmp/dygraph.js
 
-java -jar custom_rhino.jar -c /tmp/dygraph.js \
+java -jar yuicompressor-2.4.2.jar /tmp/dygraph.js \
 > /tmp/dygraph-packed.js
 
 # TODO(danvk): ensure the dygraphs copyright, etc. gets into the packed js.
 
 cat \
-strftime/strftime-min.js \
 /tmp/dygraph-packed.js \
 > dygraph-combined.js
