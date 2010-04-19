ff533c1036531c10279a83e5b3cd112dd1afd706
Dan Vanderkam <danvdk@gmail.com>
2010-04-19 15:42:31 -0700

    push-to-web.sh

---
+++ push-to-web.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script generates the combined JS file, pushes all content to a web site
+# and then reverts the combined file.
+site=$1
+
+# Produce dygraph-combined.js.
+./generate-combined.sh
+
+# Copy everything to the site.
+scp tests/*.html tests/*.js $site/tests/ \
+&& \
+scp dygraph*.js gadget.xml excanvas.js thumbnail.png docs/* $site/
+
+# Revert changes to dygraph-combined.js
+git co dygraph-combined.js
