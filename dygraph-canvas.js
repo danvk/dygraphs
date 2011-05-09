e4182459889d71f9c69157d71ca042f194b4c58f
Dan Vanderkam <dan@dygraphs.com>
2011-05-09 13:25:10 -0400

    linear/log toggle with customBars now works perfectly

--- dygraph-canvas.js
+++ dygraph-canvas.js
@@ -852,7 +852,7 @@ DygraphCanvasRenderer.prototype._renderLineChart = function() {
   // TODO(danvk): use this.attr_ for many of these.
   var context = this.elementContext;
   var fillAlpha = this.attr_('fillAlpha');
-  var errorBars = this.attr_("errorBars");
+  var errorBars = this.attr_("errorBars") || this.attr_("customBars");
   var fillGraph = this.attr_("fillGraph");
   var stackedGraph = this.attr_("stackedGraph");
   var stepPlot = this.attr_("stepPlot");
