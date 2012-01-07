5f303314912efce4e8b4ab2429c2348e76f3bda4
Robert Konigsberg <konigsberg@google.com>
2012-01-06 19:48:19 -0500

    Add experimental, add usage test.

--- push-to-web.sh
+++ push-to-web.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # This script generates the combined JS file, pushes all content to a web site
 # and then reverts the combined file.
+
+if [ "$1" == "" ] ; then
+  echo "usage: $0 URL"
+  exit 1
+fi
+
 set -x
 site=$1
-
 # Produce dygraph-combined.js.
 ./generate-combined.sh
 
@@ -12,7 +17,7 @@ site=$1
 ./generate-jsdoc.sh
 
 # Copy everything to the site.
-scp -r tests jsdoc $site \
+scp -r tests jsdoc experimental $site \
 && \
 scp dygraph*.js gadget.xml excanvas.js thumbnail.png screenshot.png docs/* $site/
 
