f924d6209abda489cade7c6c5a6295ff51c7a0dc
Russell Valentine <russ@coldstonelabs.org>
2012-01-18 00:02:31 -0600

    small legend cleanup.

--- dygraph.js
+++ dygraph.js
@@ -1549,13 +1549,13 @@ Dygraph.prototype.generateLegendDashHTML_ = function(strokePattern, color) {
     dash="<div style=\"display: inline-block; position: relative; bottom: .5ex; padding-left: 1em; height: 1px; border-bottom: 2px "+strokePattern+" "+color+";\"></div>";
   } else if(strokePattern == 'dashed') {
     dash="<div style=\"display: inline-block; position: relative; bottom: .5ex; padding-left: .4em; height: 1px; border-bottom: 2px solid "+color+";\"></div>"+
-    "<div style=\"display: inline-block; position: relative; bottom: .5ex; margin-left: .2em; height: 1px; border-bottom: 2px solid "+color+";\"></div>"+
+    "<div style=\"display: inline-block; margin-left: .2em; height: 1px\"></div>"+
     "<div style=\"display: inline-block; position: relative; bottom: .5ex; padding-left: .4em; height: 1px; border-bottom: 2px solid "+color+";\"></div>";
   } else if(strokePattern === 'dashdotted') {
     dash="<div style=\"display: inline-block; position: relative; bottom: .5ex; padding-left: .3em; height: 1px; border-bottom: 2px solid "+color+";\"></div>"+
-    "<div style=\"display: inline-block; position: relative; bottom: .5ex; margin-left: .1em; height: 1px; border-bottom: 2px solid "+color+";\"></div>"+
+    "<div style=\"display: inline-block; margin-left: .1em; height: 1px;\"></div>"+
     "<div style=\"display: inline-block; position: relative; bottom: .5ex; padding-left: .1em; height: 1px; border-bottom: 2px solid "+color+";\"></div>"+
-    "<div style=\"display: inline-block; position: relative; bottom: .5ex; margin-left: .1em; height: 1px; border-bottom: 2px solid "+color+";\"></div>"+
+    "<div style=\"display: inline-block; margin-left: .1em; height: 1px;\"></div>"+
     "<div style=\"display: inline-block; position: relative; bottom: .5ex; padding-left: .3em; height: 1px; border-bottom: 2px solid "+color+";\"></div>";
   } else {
     dash="<div style=\"display: inline-block; position: relative; bottom: .5ex; padding-left: 1em; height: 1px; border-bottom: 2px solid "+color+";\"></div>";
