3447a9454de0ae7b6b76f0e0ce9a3ad29c7d9b53
Robert Konigsberg <konigsberg@google.com>
2012-02-22 18:45:48 -0500

    whitespace cleanup

--- auto_tests/tests/callback.js
+++ auto_tests/tests/callback.js
@@ -20,35 +20,35 @@ CallbackTestCase.prototype.tearDown = function() {
  "13,0,2,3\n";
  
  
- /**
-  * This tests that when the function idxToRow_ returns the proper row and the onHiglightCallback
-  * is properly called when the  first series is hidden (setVisibility = false) 
-  * 
-  */
- CallbackTestCase.prototype.testHighlightCallbackIsCalled = function() {
-   var h_row;
-   var h_pts;
+/**
+ * This tests that when the function idxToRow_ returns the proper row and the onHiglightCallback
+ * is properly called when the  first series is hidden (setVisibility = false) 
+ * 
+ */
+CallbackTestCase.prototype.testHighlightCallbackIsCalled = function() {
+  var h_row;
+  var h_pts;
 
-   var highlightCallback  =  function(e, x, pts, row) {
-   	  h_row = row;
-   	  h_pts = pts;
-   }; 
+  var highlightCallback  =  function(e, x, pts, row) {
+    h_row = row;
+    h_pts = pts;
+  }; 
 
    
 
-   var graph = document.getElementById("graph");
-   var g = new Dygraph(graph, data,
-       {
-         width: 100,
-         height : 100,
-         visibility: [false, true, true],
-         highlightCallback : highlightCallback,
-       });
+  var graph = document.getElementById("graph");
+  var g = new Dygraph(graph, data,
+      {
+        width: 100,
+        height : 100,
+        visibility: [false, true, true],
+        highlightCallback : highlightCallback,
+      });
 
-   DygraphOps.dispatchMouseMove(g, 13, 10);
+  DygraphOps.dispatchMouseMove(g, 13, 10);
 
-   //check correct row is returned
-   assertEquals(3, h_row);
-   //check there are only two points (because first series is hidden)
-   assertEquals(2, h_pts.length);
- };
+  //check correct row is returned
+  assertEquals(3, h_row);
+  //check there are only two points (because first series is hidden)
+  assertEquals(2, h_pts.length);
+};
