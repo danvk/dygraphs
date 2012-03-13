danvk/dygraphs

commit 769e8bc7b8799385e2677b26a5c0a72e839f44ca

Fix issue 237 (standard date/time string)
This incorporates a patch from Steve Flenniken to support ISO 8601-format
(YYYY-MM-DDTHH:MM:SS.ddddddZ) dates. Also includes a regression test.

dygraph-utils.js
@@ -465,6 +465,11 @@ Dygraph.binarySearch = function(val, arry, abs, low, high) {
 Dygraph.dateParser = function(dateStr) {
   var dateStrSlashed;
   var d;
+
+  // Let the system try the format first.
+  d = Dygraph.dateStrToMillis(dateStr);
+  if (d && !isNaN(d)) return d;
+
   if (dateStr.search("-") != -1) {  // e.g. '2009-7-12' or '2009-07-12'
     dateStrSlashed = dateStr.replace("-", "/", "g");
     while (dateStrSlashed.search("-") != -1) {

auto_tests/misc/local.html
@@ -34,6 +34,7 @@
   <script type="text/javascript" src="../tests/annotations.js"></script>
   <script type="text/javascript" src="../tests/scientific_notation.js"></script>
   <script type="text/javascript" src="../tests/pathological_cases.js"></script>
+  <script type="text/javascript" src="../tests/date_formats.js"></script>
   <script type="text/javascript" src="../tests/update_options.js"></script>
   <script type="text/javascript" src="../tests/utils_test.js"></script>
   <script type="text/javascript" src="../tests/multiple_axes.js"></script>

auto_tests/tests/date_formats.js
@@ -1,0 +1,35 @@
+/**
+ * @fileoverview Tests that various formats of date are understood by dygraphs.
+ *
+ * @author dan@dygraphs.com (Dan Vanderkam)
+ */
+var dateFormatsTestCase = TestCase("date-formats");
+
+dateFormatsTestCase.prototype.setUp = function() {
+};
+
+dateFormatsTestCase.prototype.tearDown = function() {
+};
+
+dateFormatsTestCase.prototype.testISO8601 = function() {
+  // Format: YYYY-MM-DDTHH:MM:SS.ddddddZ
+  // The "Z" indicates UTC, so this test should pass regardless of the time
+  // zone of the machine on which it is run.
+  assertEquals(946816496789, Dygraph.dateParser("2000-01-02T12:34:56.789012Z"));
+};
+
+dateFormatsTestCase.prototype.testHyphenatedDate = function() {
+  // Format: YYYY-MM-DD HH:MM
+
+  // Midnight February 2, 2000, UTC
+  var d = new Date(Date.UTC(2000, 1, 2));
+
+  // Convert to a string in the local time zone: YYYY-DD-MM HH:MM
+  var zp = function(x) { return x < 10 ? '0' + x : x; };
+  var str = d.getFullYear() + '-' +
+            zp(1 + d.getMonth()) + '-' +
+            zp(d.getDate()) + ' ' +
+            zp(d.getHours()) + ':' +
+            zp(d.getMinutes());
+  assertEquals(Date.UTC(2000, 1, 2), Dygraph.dateParser(str));
+};

dygraph.js
@@ -750,6 +750,7 @@ Dygraph.prototype.numRows = function() {
 /**
  * Returns the full range of the x-axis, as determined by the most extreme
  * values in the data set. Not affected by zooming, visibility, etc.
+ * TODO(danvk): merge w/ xAxisExtremes
  * @return { Array<Number> } A [low, high] pair
  * @private
  */

commit 3f675fe524eece31f39fb68601d2d2d7d5720941

Fix issue 255: date supplied as YYYY-MM-DD shows previous day's date...
JavaScript date parsing is stunningly complex. This was broken by
https://github.com/danvk/dygraphs/commit/769e8bc7b8799385e2677b26a5c0a72e839f44ca

The underlying issue is that new Date("YYYY-MM-DD") parses the date in
UTC, but new Date("YYYY/MM/DD") parses it in the local time zone.

dygraph-utils.js
@@ -494,9 +494,17 @@ Dygraph.dateParser = function(dateStr) {
   var dateStrSlashed;
   var d;
 
-  // Let the system try the format first.
-  d = Dygraph.dateStrToMillis(dateStr);
-  if (d && !isNaN(d)) return d;
+  // Let the system try the format first, with one caveat:
+  // YYYY-MM-DD[ HH:MM:SS] is interpreted as UTC by a variety of browsers.
+  // dygraphs displays dates in local time, so this will result in surprising
+  // inconsistencies. But if you specify "T" or "Z" (i.e. YYYY-MM-DDTHH:MM:SS),
+  // then you probably know what you're doing, so we'll let you go ahead.
+  // Issue: http://code.google.com/p/dygraphs/issues/detail?id=255
+  if (dateStr.search("-") == -1 ||
+      dateStr.search("T") != -1 || dateStr.search("Z") != -1) {
+    d = Dygraph.dateStrToMillis(dateStr);
+    if (d && !isNaN(d)) return d;
+  }
 
   if (dateStr.search("-") != -1) {  // e.g. '2009-7-12' or '2009-07-12'
     dateStrSlashed = dateStr.replace("-", "/", "g");

auto_tests/misc/local.html
@@ -37,6 +37,7 @@
   <script type="text/javascript" src="../tests/date_formats.js"></script>
   <script type="text/javascript" src="../tests/formats.js"></script>
   <script type="text/javascript" src="../tests/update_while_panning.js"></script>
+  <script type="text/javascript" src="../tests/no_hours.js"></script>
   <script type="text/javascript" src="../tests/update_options.js"></script>
   <script type="text/javascript" src="../tests/utils_test.js"></script>
   <script type="text/javascript" src="../tests/multiple_axes.js"></script>

auto_tests/tests/no_hours.js
@@ -1,0 +1,77 @@
+/**
+ * @fileoverview Tests that we don'show specify hours, minutes or seconds
+ * in your dates if you don't specify them. This can get mixed up becaues of
+ * time zones.
+ *
+ * @author danvk@google.com (Dan Vanderkam)
+ */
+var noHoursTestCase = TestCase("no-hours");
+
+noHoursTestCase.prototype.setUp = function() {
+  document.body.innerHTML = "<div id='graph'></div>";
+};
+
+noHoursTestCase.prototype.tearDown = function() {
+};
+
+function getLegend() {
+  var legend = document.getElementsByClassName("dygraph-legend")[0];
+  return legend.textContent;
+}
+
+noHoursTestCase.prototype.testNoHours = function() {
+  var opts = {
+    width: 480,
+    height: 320
+  };
+  var data = "Date,Y\n" +
+      "2012/03/13,-1\n" +
+      "2012/03/14,0\n" +
+      "2012/03/15,1\n" +
+      "2012/03/16,0\n"
+  ;
+
+  var graph = document.getElementById("graph");
+  var g = new Dygraph(graph, data, opts);
+
+  g.setSelection(0);
+  assertEquals("2012/03/13: Y:-1", getLegend());
+
+  g.setSelection(1);
+  assertEquals("2012/03/14: Y:0", getLegend());
+
+  g.setSelection(2);
+  assertEquals("2012/03/15: Y:1", getLegend());
+
+  g.setSelection(3);
+  assertEquals("2012/03/16: Y:0", getLegend());
+};
+
+noHoursTestCase.prototype.testNoHoursDashed = function() {
+  var opts = {
+    width: 480,
+    height: 320
+  };
+  var data = "Date,Y\n" +
+      "2012-03-13,-1\n" +
+      "2012-03-14,0\n" +
+      "2012-03-15,1\n" +
+      "2012-03-16,0\n"
+  ;
+
+  var graph = document.getElementById("graph");
+  var g = new Dygraph(graph, data, opts);
+
+  g.setSelection(0);
+  assertEquals("2012/03/13: Y:-1", getLegend());
+
+  g.setSelection(1);
+  assertEquals("2012/03/14: Y:0", getLegend());
+
+  g.setSelection(2);
+  assertEquals("2012/03/15: Y:1", getLegend());
+
+  g.setSelection(3);
+  assertEquals("2012/03/16: Y:0", getLegend());
+};
+