3f55b813316f73082795fe52f6cf26e9f1592f12
Klaus Weidner <klausw@google.com>
2012-07-31 12:07:34 -0700

    Export .isSeriesLocked() API function
    This is a followup to https://github.com/danvk/dygraphs/pull/159,
    exposing the .lockedSet_ internal attribute through the API
    for use in callbacks, and also with an eye for future extraction
    of the mouseover logic into a plugin.
    
    Includes a fix for the auto_test name, it was erroneously replacing
    the existing testClosestPointCallbackCss1 test due to a name clash.

--- dygraph.js
+++ dygraph.js
@@ -1734,7 +1734,7 @@ Dygraph.prototype.mouseMove_ = function(event) {
 
   var highlightSeriesOpts = this.attr_("highlightSeriesOpts");
   var selectionChanged = false;
-  if (highlightSeriesOpts && !this.lockedSet_) {
+  if (highlightSeriesOpts && !this.isSeriesLocked()) {
     var closest;
     if (this.attr_("stackedGraph")) {
       closest = this.findStackedPoint(canvasx, canvasy);
@@ -2022,6 +2022,14 @@ Dygraph.prototype.getSelection = function() {
  */
 Dygraph.prototype.getHighlightSeries = function() {
   return this.highlightSet_;
+};
+
+/**
+ * Returns true if the currently-highlighted series was locked
+ * via setSelection(..., seriesName, true).
+ */
+Dygraph.prototype.isSeriesLocked = function() {
+  return this.lockedSet_;
 };
 
 /**
