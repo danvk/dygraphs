fa460473ef9397759466361ff32de56a4f8fa956
Klaus Weidner <klausw@google.com>
2013-02-08 17:01:10 -0800

    Add new options xRangePad and yRangePad
    These options provide a fixed margin (in pixels) around the
    user-specified or autoselected axis range to ensure all data points are
    fully visible. If these are not set by the user, behavior reverts to
    backward compatible mode.
    
    This helps avoid the issue of data points drawn at the graph edges not
    being properly visible.  For example, in a 100-pixel high plot area, a Y
    coordinate of zero on a graph with yrange [0, ...] will be scaled to
    domY=100 which is outside the 0..99 visible area.
    
    If I'm understanding it right, the original padding algorithm works
    as follows:
    
      Add 10% padding for automatic Y ranges, but not for user-supplied
      ranges, and move a close-to-zero edge to zero except if avoidMinZero
      is set. If logscale is set, add a variable amount of padding at the
      top but none at the bottom.
    
    This is not very intuitive, and lines drawn at the edge of a
    user-supplied range will still be invisible.
    
    The new algorithm consistently ensures that there are at least
    {x,y}RangePad pixels available for drawing outside of the data range,
    both for user-specified and autoselected ranges.
    
    Setting a small xRangePad combines nicely with drawAxesAtZero,
    it will provide tick marks next to the legend due to the grid extending
    slightly past the axis.
    
    See the included gallery demo 'Edge Padding'. The default setting
    corresponds to legacy behavior. Recommended settings:
      includeZero: true,
      drawAxesAtZero: true,
      xRangePad: 4,
      yRangePad: 10

--- auto_tests/tests/pathological_cases.js
+++ auto_tests/tests/pathological_cases.js
@@ -36,6 +36,86 @@ pathologicalCasesTestCase.prototype.testOnePoint = function() {
   var g = new Dygraph(graph, data, opts);
 };
 
+pathologicalCasesTestCase.prototype.testCombinations = function() {
+  var dataSets = {
+    empty: [],
+    onePoint: [[10, 2]],
+    nanPoint: [[10, NaN]],
+    nanPoints: [[10, NaN], [20, NaN]],
+    multiNan1: [[10, NaN, 2], [20, 3, NaN]],
+    multiNan2: [[10, NaN, 2], [20, NaN, 4]],
+    multiNan3: [[10, NaN, NaN], [20, 3, 4], [30, NaN, NaN]],
+    atZero: [[0, 0]],
+    atZero2: [[0, 0, 0]],
+    negative: [[-10, -1]],
+    acrossZero: [[-10, 1], [10, 2]],
+    normal: [[0,1,9], [10,3,5], [20,2,7], [30,4,3]]
+  };
+
+  var baseOpts = {
+    lines: {},
+    stacked: {
+      stackedGraph: true
+    }
+  };
+
+  var variantOpts = {
+    none: {},
+    avoidMinZero: {
+      avoidMinZero: true,
+      includeZero: true
+    },
+    padded: {
+      includeZero: true,
+      drawAxesAtZero: true,
+      xRangePad: 0.02,
+      yRangePad: 0.04
+    }
+  };
+
+  for (var baseName in baseOpts) {
+    var base = baseOpts[baseName];
+    for (var variantName in variantOpts) {
+      var variant = variantOpts[variantName];
+
+      var opts = {
+        width: 300,
+        height: 150,
+        labelsDivWidth: 100,
+        pointSize: 10
+      };
+      for (var key in base) {
+        if (base.hasOwnProperty(key)) opts[key] = base[key];
+      }
+      for (var key in variant) {
+        if (variant.hasOwnProperty(key)) opts[key] = variant[key];
+      }
+
+      var h = document.createElement('h3');
+      h.appendChild(document.createTextNode(baseName + ' ' + variantName));
+      document.body.appendChild(h);
+      for (var dataName in dataSets) {
+        var data = dataSets[dataName];
+
+        var box = document.createElement('fieldset');
+        box.style.display = 'inline-block';
+        var legend = document.createElement('legend');
+        legend.appendChild(document.createTextNode(dataName));
+        box.appendChild(legend);
+        var gdiv = document.createElement('div');
+        gdiv.style.display = 'inline-block';
+        box.appendChild(gdiv);
+        document.body.appendChild(box);
+
+        var cols = data && data[0] ? data[0].length : 0;
+        opts.labels = ['X', 'A', 'B', 'C'].slice(0, cols);
+
+        var g = new Dygraph(gdiv, data, opts);
+      }
+    }
+  }
+};
+
 pathologicalCasesTestCase.prototype.testNullLegend = function() {
   var opts = {
     width: 480,
