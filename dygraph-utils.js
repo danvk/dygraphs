5ad7495d3176ab8f7e79304be352ffe1e24bfb35
Dan Vanderkam <danvk@google.com>
2013-02-19 15:55:35 -0500

    getComputedStyle -> window.getComputedStyle

--- dygraph-utils.js
+++ dygraph-utils.js
@@ -301,7 +301,7 @@ Dygraph.findPosX = function(obj) {
   if(obj.offsetParent) {
     var copyObj = obj;
     while(1) {
-      var borderLeft = getComputedStyle(copyObj, null).borderLeft || "0";
+      var borderLeft = window.getComputedStyle(copyObj, null).borderLeft || "0";
       curleft += parseInt(borderLeft, 10) ;
       curleft += copyObj.offsetLeft;
       if(!copyObj.offsetParent) {
@@ -334,7 +334,7 @@ Dygraph.findPosY = function(obj) {
   if(obj.offsetParent) {
     var copyObj = obj;
     while(1) {
-      var borderTop = getComputedStyle(copyObj, null).borderTop || "0";
+      var borderTop = window.getComputedStyle(copyObj, null).borderTop || "0";
       curtop += parseInt(borderTop, 10) ;
       curtop += copyObj.offsetTop;
       if(!copyObj.offsetParent) {
