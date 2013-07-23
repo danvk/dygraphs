555955569d83985e1c919a76fa831bc743fcec6e
Dan Vanderkam <danvdk@gmail.com>
2013-07-22 20:10:31 -0400

    start using export symbols

---
+++ dygraph-exports.js
@@ -0,0 +1,3 @@
+goog.exportSymbol('Dygraph', Dygraph);
+goog.exportSymbol('Dygraph.prototype.updateOptions', Dygraph.prototype.updateOptions);
+goog.exportSymbol('Dygraph.prototype.destroy', Dygraph.prototype.destroy);
