382ab90d75d349cfbc966f535422aad862deb453
Dan Vanderkam <danvdk@gmail.com>
2014-06-25 00:09:53 -0400

    Bug fix for horizontal scrolling on hidpi devices

--- dygraph.js
+++ dygraph.js
@@ -1136,6 +1136,7 @@ Dygraph.prototype.createInterface_ = function() {
 
   // TODO(danvk): any other styles that are useful to set here?
   this.graphDiv.style.textAlign = 'left';  // This is a CSS "reset"
+  this.graphDiv.style.position = 'relative';
   enclosing.appendChild(this.graphDiv);
 
   // Create the canvas for interactive parts of the chart.
