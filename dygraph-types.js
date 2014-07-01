7af51ec0ce747cac5a0eb71d0af9444c5c479218
Brent Walther <brentwalther@google.com>
2014-07-01 15:57:17 -0700

    Add Dygraph.PointType to dygraph-types
    Add the Dygraph.PointType type to the dygraph-types externs file. This type is used when defining a clickCallback function and must be defined in order to satisfy the Google Closure compiler.

--- dygraph-types.js
+++ dygraph-types.js
@@ -14,3 +14,27 @@
  */
 var DygraphInteractionContext;
 
+/**
+ * Point structure.
+ *
+ * xval_* and yval_* are the original unscaled data values,
+ * while x_* and y_* are scaled to the range (0.0-1.0) for plotting.
+ * yval_stacked is the cumulative Y value used for stacking graphs,
+ * and bottom/top/minus/plus are used for error bar graphs.
+ *
+ * @typedef {{
+ *     idx: number,
+ *     name: string,
+ *     x: ?number,
+ *     xval: ?number,
+ *     y_bottom: ?number,
+ *     y: ?number,
+ *     y_stacked: ?number,
+ *     y_top: ?number,
+ *     yval_minus: ?number,
+ *     yval: ?number,
+ *     yval_plus: ?number,
+ *     yval_stacked
+ * }}
+ */
+Dygraph.PointType;
