975dc4efd4225497e5067a619c0828d9d135d3c9
Jeff VanDyke <jeffvd.a216@gmail.com>
2017-09-01 16:08:17 -0400

    Added note about pixelRatio affecting resolution

--- src/dygraph-options-reference.js
+++ src/dygraph-options-reference.js
@@ -303,7 +303,7 @@ OPTIONS_REFERENCE =  // <JSON>
     "default": "(devicePixelRatio / context.backingStoreRatio)",
     "labels": ["Overall display"],
     "type": "float",
-    "description": "Overrides the pixel ratio scaling factor for the canvas's 2d context. Ordinarily, this is set to the devicePixelRatio / (context.backingStoreRatio || 1), so on mobile devices, where the devicePixelRatio can be somewhere around 3, performance can be improved by overriding this value to something less precise, like 1."
+    "description": "Overrides the pixel ratio scaling factor for the canvas's 2d context. Ordinarily, this is set to the devicePixelRatio / (context.backingStoreRatio || 1), so on mobile devices, where the devicePixelRatio can be somewhere around 3, performance can be improved by overriding this value to something less precise, like 1, at the expense of resolution."
   },
   "interactionModel": {
     "default": "...",
