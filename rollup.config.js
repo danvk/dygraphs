b98061df391d33d8696279b3aa4c0365be9c7408
Leon Sorokin <leeoniya@gmail.com>
2018-11-20 21:59:53 -0600

    replace babel & browserify with rollup

---
+++ rollup.config.js
@@ -0,0 +1,60 @@
+import replace from 'rollup-plugin-re';
+import { terser } from "rollup-plugin-terser";
+
+const banner = '/*! @license Copyright 2017 Dan Vanderkam (danvdk@gmail.com) MIT-licensed (http://opensource.org/licenses/MIT) */';
+
+module.exports = [
+	{
+		input: 'index.js',
+		output: {
+			banner: banner,
+			sourcemap: true,
+			file: 'dist/dygraph.js',
+			name: 'Dygraph',
+			format: 'umd'
+		},
+		plugins: [
+			replace({
+				patterns: [
+					{
+						test: /typeof\(process\)/gm,
+						replace: 'true',
+					},
+					{
+						test: /process\.env\.NODE_ENV/gm,
+						replace: "'development'",
+					}
+				]
+			}),
+		]
+	},
+	{
+		input: 'index.js',
+		output: {
+			banner: banner,
+			sourcemap: true,
+			file: 'dist/dygraph.min.js',
+			name: 'Dygraph',
+			format: 'umd'
+		},
+		plugins: [
+			replace({
+				patterns: [
+					{
+						test: /typeof\(process\)/gm,
+						replace: "'undefined'",
+					},
+					{
+						test: /process\.env\.NODE_ENV/gm,
+						replace: "'production'",
+					}
+				]
+			}),
+			terser({
+				output: {
+					comments: /^!/
+				}
+			}),
+		]
+	}
+];
