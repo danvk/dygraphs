1f04c4af3934e21728ba2f16622d81b06ab611dc
mirabilos <t.glaser@tarent.de>
2022-09-15 01:14:22 +0200

    just use U+00A0 directly

--- auto_tests/tests/Util.js
+++ auto_tests/tests/Util.js
@@ -56,8 +56,7 @@ Util.getClassTexts = function(css_class, parent) {
 
 // Convert &nbsp; to a normal space
 Util.nbspToSpace = function(str) {
-  var re = new RegExp(String.fromCharCode(160), 'g');
-  return str.replace(re, ' ');
+  return str.replace(/ /g, ' ');
 };
 
 Util.getLegend = function(parent) {
