e6b6787f6d931075665ce7ba110e4863e748e387
mirabilos <t.glaser@tarent.de>
2022-10-19 18:12:53 +0200

    draft a less complicated buildsystem

---
+++ scripts/b-new.sh
@@ -0,0 +1,42 @@
+#!/bin/mksh
+set -e
+header=$1
+
+rm -rf disttmp2
+mkdir disttmp2
+cp LICENCE.js disttmp2/
+
+babel --compact false -d disttmp2/es5 src
+
+cd disttmp2
+
+cp -r es5 src
+browserify \
+  -v \
+  -t [ envify --NODE_ENV development ] \
+  --debug \
+  --standalone Dygraph \
+  LICENCE.js \
+  src/dygraph.js \
+  >dygraph.tmp.js
+exorcist --base . dygraph.js.map <dygraph.tmp.js >dygraph.js
+
+#cp -r es5 src
+browserify \
+  -v \
+  -t [ envify --NODE_ENV production ] \
+  --debug \
+  --standalone Dygraph \
+  src/dygraph.js \
+  >dygraph.min.tmp.js
+exorcist --base . dygraph.min.tmp.js.map <dygraph.min.tmp.js >/dev/null
+
+uglifyjs --compress --mangle \
+  --preamble "$header" \
+  --in-source-map dygraph.min.tmp.js.map \
+  --source-map-include-sources \
+  --source-map dygraph.min.js.map \
+  -o dygraph.min.js \
+  dygraph.min.tmp.js
+
+cd ..
