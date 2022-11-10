d04cca7e68dbf686aa3c0b8c3ab9b32ce57eee52
mirabilos <t.glaser@tarent.de>
2022-11-10 22:56:44 +0100

    cope with nodejs being called node

--- scripts/env-patcher.sh
+++ scripts/env-patcher.sh
@@ -14,10 +14,16 @@ else
 fi
 shift
 
+if command -v nodejs >/dev/null 2>&1; then
+	node_js=nodejs
+else
+	node_js=node
+fi
+
 grep -FrlZ process.env.NODE_ENV "$@" | while IFS= read -d '' -r fn; do
 	print -ru2 "I: patching $fn for !prod=$rpl"
 	"$mydir"/smap-out.py "$fn" env-patcher.tmp.js env-patcher.tmp.map
-	nodejs "$mydir"/env-patcher.js "$rpl"
+	$node_js "$mydir"/env-patcher.js "$rpl"
 	"$mydir"/smap-in.py env-patcher.tmp.js env-patcher.tmp.map "$fn" --nonl
 done
 rm -f env-patcher.tmp.js env-patcher.tmp.map
