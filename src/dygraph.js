69ac747f744fffff40bc3b4e26df6f4363ad1e41
mirabilos <t.glaser@tarent.de>
2022-11-29 21:16:18 +0100

    next is 2.1.2~alpha

--- src/dygraph.js
+++ src/dygraph.js
@@ -92,7 +92,7 @@ var Dygraph = function(div, data, opts) {
 };
 
 Dygraph.NAME = "Dygraph";
-Dygraph.VERSION = "2.1.1";
+Dygraph.VERSION = "2.1.2-alpha";
 
 // Various default values
 Dygraph.DEFAULT_ROLL_PERIOD = 1;
