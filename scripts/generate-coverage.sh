f19b2beb7f9b070aefaf3794c2f17f06b6f679b7
mirabilos <tg@debian.org>
2025-01-10 22:25:56 +0100

    show a message about ignoring errors to post to coveralls.io service (neither that nor the weigh-in.sh thingy have worked for a while, but they’re danvk’s so keeping them in harmlessly)

--- scripts/generate-coverage.sh
+++ scripts/generate-coverage.sh
@@ -54,7 +54,7 @@ phantomjs \
   http://localhost:8082/auto_tests/coverage.html \
   spec '{"hooks": "mocha-phantomjs-istanbul", "coverageFile": "coverage/coverage.json"}'
 
-if [ $CI ]; then
+if test -n "$CI"; then
   # Convert the JSON coverage to LCOV for coveralls.
   istanbul report --include coverage/*.json lcovonly
 
--- scripts/post-coverage.sh
+++ scripts/post-coverage.sh
@@ -4,8 +4,9 @@
 # comment the line out to post
 #exit 0
 
-if [ $CI ]; then
-  <coverage/lcov.info ./node_modules/.bin/coveralls
+if test -n "$CI"; then
+  <coverage/lcov.info ./node_modules/.bin/coveralls || \
+    echo "N: above errors posting to coveralls.io are ignored"
 fi
 
 true  # reset exit code -- failure to post coverage shouldn't be an error.
